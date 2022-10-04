c37e1c03326f87ac6cbd19d608d00e0ad94e6116
ganguly-s <gangulyshalini1@gmail.com>
2022-10-03 19:00:27 -0700

    gal_center module added, to compute center of galaxy from ra,dec

--- main.py
+++ main.py
@@ -18,17 +18,27 @@ from astropy.wcs import utils
 gname = '2A0335'
 telescope = 'MUSE'
 # telescope = 'ALMA'
-# pltmap = 'velocity'
-pltmap = 'flux'
+pltmap = 'velocity'
+# pltmap = 'flux'
 max_error = 5
 
 sysparam = inp.get_sys_params(gname,telescope,pltmap)
 
+# extracting BH location
+def gal_center(fn):
+    hdu = fits.open(fn)[0]
+    wcs = WCS(hdu.header)
+    c = SkyCoord(ra+' '+dec, unit=(u.hourangle, u.deg))
+    target = utils.skycoord_to_pixel(c, wcs)
+    Cx, Cy = target[0],target[1]
+    print("center:",Cx,Cy)
+    return Cx, Cy
+
 if telescope=='MUSE':
     if pltmap=='velocity':
-        fnvel = sysparam['vmapfn']
-        fnvelerr = sysparam['verrmapfn']
-        fnflux = sysparam['fluxmapfn']
+        fnvel = 'inputs/'+sysparam['vmapfn']
+        fnvelerr = 'inputs/'+sysparam['verrmapfn']
+        fnflux = 'inputs/'+sysparam['fluxmapfn']
         flcut = sysparam['flcut']
         ra = sysparam['ra']
         dec = sysparam['dec']
@@ -45,8 +55,24 @@ if telescope=='MUSE':
         vsfyl, vsfyu = sysparam['vsfyl'], sysparam['vsfyu']
         histulim, sepbin = sysparam['histulim'], sysparam['sepbin']
         res = res*0.2  # constant sampling size is 0.2"
+        # data extraction and plotting
+        velo = fits.open(fnvel)
+        velo_data = velo[0].data
+        velo.close()
+        error = fits.open(fnvelerr)
+        error_data = error[0].data
+        error.close()
+        flux = fits.open(fnflux)
+        flux_data = flux[0].data
+        flux.close()
+        Cx, Cy = gal_center(fnvel)
+        alldata = {'gname':gname,'velo_data':velo_data,'error_data':error_data,
+                   'flux_data':flux_data,'flcut':flcut,'rkpc':rkpc,'mask_size':msk_sz,
+                   'Cx':Cx,'Cy':Cy,'ylab':ylab,'xlab':xlab,'res':res,
+                   'xpl':xpl, 'xpu':xpu, 'ypl':ypl, 'ypu':ypu, 'cbfrac':cbfrac}
+        #plotter.plot_data(alldata, max_error, telescope, pltmap)
     if pltmap=='flux':
-        fnflux = sysparam['fluxmapfn']
+        fnflux = 'inputs/'+sysparam['fluxmapfn']
         flcut = sysparam['flcut']
         ra = sysparam['ra']
         dec = sysparam['dec']
@@ -63,9 +89,17 @@ if telescope=='MUSE':
         vsfyl, vsfyu = sysparam['vsfyl'], sysparam['vsfyu']
         histulim, sepbin = sysparam['histulim'], sysparam['sepbin']
         res = res*0.2  # constant sampling size is 0.2"
+        # data extraction and plotting
+        flux = fits.open(fnflux)
+        flux_data = flux[0].data
+        flux.close()
+        alldata = {'gname':gname,'flux_data':flux_data,'flcut':flcut,
+                   'mask_size':msk_sz,'Cx':Cx,'Cy':Cy,'ylab':ylab,'xlab':xlab,
+                   'res':res,'xpl':xpl,'xpu':xpu,'ypl':ypl,'ypu':ypu,'cbfrac':cbfrac}
+        #plotter.plot_data(alldata, max_error, telescope, pltmap)
 if telescope=='ALMA':
     if pltmap=='velocity':
-        fnvel = sysparam['vmapfn']
+        fnvel = 'inputs/'+sysparam['vmapfn']
         ra = sysparam['ra']
         dec = sysparam['dec']
         res = sysparam['res']
@@ -83,41 +117,4 @@ if telescope=='ALMA':
         n_bins, cd = sysparam['n_bins'], sysparam['cd']
         res = res*cd # sampling size changes for each source
     # if pltmap=='flux':
-
-## extracting BH location
-if pltmap=='velocity':
-    filename = get_pkg_data_filename(fnvel)
-if pltmap=='flux':
-    filename = get_pkg_data_filename(fnflux)
-hdu = fits.open(filename)[0]
-wcs = WCS(hdu.header)
-c = SkyCoord(ra+' '+dec, unit=(u.hourangle, u.deg))
-target = utils.skycoord_to_pixel(c, wcs)
-Cx, Cy = target[0],target[1]
-print(Cx,Cy)
-
-## plotting the velocity maps with/without masks
-if telescope=='MUSE':
-    if pltmap=='velocity':
-        velo = fits.open(fnvel)
-        velo_data = velo[0].data
-        velo.close()
-        error = fits.open(fnvelerr)
-        error_data = error[0].data
-        error.close()
-        flux = fits.open(fnflux)
-        flux_data = flux[0].data
-        flux.close()
-        alldata = {'gname':gname,'velo_data':velo_data,'error_data':error_data,
-                   'flux_data':flux_data,'flcut':flcut,'rkpc':rkpc,'mask_size':msk_sz,
-                   'Cx':Cx,'Cy':Cy,'ylab':ylab,'xlab':xlab,'res':res,
-                   'xpl':xpl, 'xpu':xpu, 'ypl':ypl, 'ypu':ypu, 'cbfrac':cbfrac}
-        good_v = plotter.plot_data(alldata, max_error, telescope, pltmap)
-    if pltmap=='flux':
-        flux = fits.open(fnflux)
-        flux_data = flux[0].data
-        flux.close()
-        alldata = {'gname':gname,'flux_data':flux_data,'flcut':flcut,
-                   'mask_size':msk_sz,'Cx':Cx,'Cy':Cy,'ylab':ylab,'xlab':xlab,
-                   'res':res,'xpl':xpl,'xpu':xpu,'ypl':ypl,'ypu':ypu,'cbfrac':cbfrac}
-        good_v = plotter.plot_data(alldata, max_error, telescope, pltmap)
+        
